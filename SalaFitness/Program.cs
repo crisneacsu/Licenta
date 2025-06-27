535c8ce4491f6a59ce71fb83339b9cbfad64959e
crisneacsu <145997878+crisneacsu@users.noreply.github.com>
2025-06-27 23:45:18 +0300

    Add files via upload

---
+++ SalaFitness/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using SalaFitness.Context;
+using Stripe;
+using Microsoft.OpenApi.Models;    
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services
+    .AddControllersWithViews()
+    .AddJsonOptions(options =>
+    {
+        
+        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    });
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        
+        options.LoginPath = "/Users/Login";
+        options.LogoutPath = "/Users/Logout";
+        options.ExpireTimeSpan = TimeSpan.FromHours(1); 
+    });
+
+
+StripeConfiguration.ApiKey = builder.Configuration["Stripe:SecretKey"];
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    ctx.Database.Migrate();
+}
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseStatusCodePagesWithReExecute("/Home/Error/{0}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
